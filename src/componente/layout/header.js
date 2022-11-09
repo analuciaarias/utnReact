434affef55431f4fadb63107ee1774c71ad14066
Ana Lucia Arias <ariasdelosheros@gmail.com>
2022-11-09 09:47:13 -0300

    primera clase react

---
+++ src/componente/layout/header.js
@@ -0,0 +1,10 @@
+const Header= (props) =>{
+    return(
+        <div>
+        <p>hola soy el header</p>
+        <p>kbwquyfuywfbnfqoincioamoaxokdoamcknckasnvkjsnvsnvnnnnnkslkasclksclnslknclasnclsanclasncklsmcalwureugbvlaksdbvauewablvxmvweifasc
+        </p>
+        </div>
+    )
+}
+export default Header;
