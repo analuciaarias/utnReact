86440952194f3721753ab11010f0532892cf2fc4
Ana Lucia Arias <ariasdelosheros@gmail.com>
2022-11-10 19:07:24 -0300

    react

--- src/pages/Ejemplo1.js
+++ src/pages/Ejemplo1.js
@@ -1,6 +1,24 @@
+import {Ejemplo1Props, Ejemplo2Props, Ejemplo3Props, Ejemplo4Props} from "../componente/props/Props";
+
+const nombre2= 'Felicitas';
+const lista = ['manzana','banana','pera','frutillas'];
+const mostrarValor= valor =>{
+    console.log(valor)
+}
 const EjemploProp01= props =>{
     return(
-        <div>Hola soy la página de ejemplo1</div>
+        <div>
+            <h1>Ejemplos de propiedades</h1>
+            <Ejemplo1Props nombre='Anita' />
+            <Ejemplo1Props nombre={nombre2} />
+            <hr/>
+            <Ejemplo2Props elementos={lista} />
+            <hr />
+            <Ejemplo3Props titulo='La noticia' subtitulo='subtitulo de la noticia' cuerpo='loremmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm' />
+            <Ejemplo4Props cambiarValor={mostrarValor} />
+        </div>
     )
 }
+
+
 export default EjemploProp01;
